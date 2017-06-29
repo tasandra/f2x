b4be2ca35e584fbd7fc4f2b61f22868e0d8456ff
tasandra <tasandra40@gmail.com>
2017-06-28 23:09:35 -0700

    filters

--- src/pages/FilterWorkout.java
+++ src/pages/FilterWorkout.java
@@ -1,16 +1,12 @@
 package pages;
 
-import static org.junit.Assert.assertTrue;
-
-import java.util.List;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
-public class SortWorkout extends HomePage {
+public class FilterWorkout extends HomePage {
 
-	public SortWorkout(WebDriver driver) {
+	public FilterWorkout(WebDriver driver) {
 		super(driver);
 	}
 
@@ -18,25 +14,16 @@ public class SortWorkout extends HomePage {
 		driver.findElement(
 				By.xpath("//*[@class = 'f2x-exercise-filter-inter']/div[5]/div[2]/button[1]"))
 				.click();
-
-		List<WebElement> absSort = driver.findElements(By.className("f2x-exercise-img-labels "));
-		for (WebElement abs : absSort) {
-			String label = abs.getText();
-			assertTrue(label.equals("Abs/Core"));
-		}
+		wait.until(ExpectedConditions
+				.visibilityOfElementLocated(By.cssSelector(".f2x-exercise-img-padding button")));
 	}
 
-	public void sortLegsAndAbs(){
+	public void sortLegsAndAbs() {
 		driver.findElement(
 				By.xpath("//*[@class = 'f2x-exercise-filter-inter']/div[5]/div[2]/button[5]"))
 				.click();
-		
-		List<WebElement> abslegsSort = driver.findElements(By.className("f2x-exercise-img-labels "));
-		for (WebElement abs : abslegsSort) {
-			String label = abs.getText();
-			assertTrue(label.equals("Abs/Core"));
-			assertTrue(label.equals("Legs") || label.equals("Abs/Core"));
-		}
+		wait.until(ExpectedConditions
+				.visibilityOfElementLocated(By.cssSelector(".f2x-exercise-img-padding button")));
 
 	}
 
--- src/test/FilterWorkoutTest.java
+++ src/test/FilterWorkoutTest.java
@@ -2,23 +2,27 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 import pages.HomePage;
-import pages.SortWorkout;
+import pages.FilterWorkout;
 import pages.WorkoutBuilder;
 
-public class SortTest extends BaseTest {
+public class FilterWorkoutTest extends BaseTest {
 	private WorkoutBuilder workout;
-	private SortWorkout sort;
+	private FilterWorkout filter;
 	private HomePage home;
 
 	@Before
 	public void setUp() throws Exception {
 		workout = new WorkoutBuilder(driver);
-		sort = new SortWorkout(driver);	
+		filter = new FilterWorkout(driver);	
 		home = new HomePage(driver);
 	}
 
@@ -30,8 +34,21 @@ public class SortTest extends BaseTest {
 	public void test() {
 		home.load();
 		workout.goToBuilder();
-		sort.sortAbs();
-		sort.sortLegsAndAbs();
+		
+		filter.sortAbs();
+		List<WebElement> absSort = driver.findElements(By.className("f2x-exercise-img-labels "));
+		for (WebElement abs : absSort) {
+			String label = abs.getText();
+			assertTrue(label.equals("Abs/Core"));
+		}
+		
+		filter.sortLegsAndAbs();
+		List<WebElement> abslegsSort = driver.findElements(By.className("f2x-exercise-img-labels "));
+		for (WebElement abs : abslegsSort) {
+			String label = abs.getText();
+			assertTrue(label.equals("Abs/Core"));
+			assertTrue(label.equals("Legs") || label.equals("Abs/Core"));
+		}
 		
 	}
 
